13a37f39e4619f4ddcf6e5e21ca3969b33ef9700
Samuel <samuelsantanadasilva8@gmail.com>
2023-02-07 17:12:11 -0300

    Alterando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 import Header from './componentes/header/header'
-import Footer from './componentes/footerCorreo/footer'
+import Footer from './componentes/footer/footer'
 import Section_um from './componentes/section_um/section_um'
 import Section_dois from './componentes/section_dois/Section_dois'
 import Sectiontres from './componentes/section_tres/section_tres'
